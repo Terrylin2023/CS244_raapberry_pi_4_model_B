35f56296133bc052103fa2e217cef5980c7ab3f9
Terrylin2023 <148923652+Terrylin2023@users.noreply.github.com>
2024-04-11 22:56:45 -0700

    Update convert_jpg_to_pgm.py

--- convert_jpg_to_pgm/convert_jpg_to_pgm.py
+++ convert_jpg_to_pgm/convert_jpg_to_pgm.py
@@ -2,14 +2,14 @@ import sys
 from PIL import Image
 
 def convert_jpg_to_pgm(jpg_file):
-    # 基於輸入檔案名稱生成PGM檔案名稱
+    # Generate PGM file name based on input file name.
     pgm_file = jpg_file.replace('.jpg', '.pgm').replace('.jpeg', '.pgm')
     
-    # 打開JPEG圖像檔案
+    # Open JPEG image files
     with Image.open(jpg_file) as img:
-        # 轉換圖像為灰階
+        # Convert image to grayscale
         img = img.convert('L')
-        # 儲存圖像為PGM格式
+        # Save images in PGM format
         img.save(pgm_file, 'PPM')
     print(f"Converted {jpg_file} to {pgm_file}")
 
@@ -17,8 +17,6 @@ if len(sys.argv) != 2:
     print("Usage: python3 convert_jpg_to_pgm.py input.jpg")
     sys.exit(1)
 
-# 命令行參數
 input_file = sys.argv[1]
 
-# 執行轉換
 convert_jpg_to_pgm(input_file)
